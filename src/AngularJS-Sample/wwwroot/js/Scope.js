ffd2a40f1ed14e22185a4189a7318a96d9fd0fbe
sunyatravel <yali.girijaa@gmail.com>
2016-11-07 13:55:19 +0530

    Angular JS
    First commit

---
+++ src/AngularJS-Sample/wwwroot/js/Scope.js
@@ -0,0 +1,7 @@
+﻿//module
+var personApp = angular.module('personApp', []);
+
+//controller 
+personApp.controller('personController', ['$scope', function ($scope) {
+    $scope.name = 'Mary Jane';
+}]);
